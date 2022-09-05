1213f915c83673114f3230e04e441293cf6b07d0
William Ribbholm <gmans9797@gmail.com>
2022-09-05 12:17:10 +0200

    Add files via upload

---
+++ Exercise1.cs
@@ -0,0 +1,13 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+static void Main(string[] args) { }
+    Console.WriteLine("Enter a number"); // Here the console says to Enter a number
+int Num = Convert.ToInt32(Console.ReadLine()); // Here is where you input your number which is defined as the Integer Num
+if (Num >= 1 && Num <= 10) // If the integer Num is greater and equal to 1 and if it is less or equal to 10
+{
+    Console.WriteLine("Valid"); //If the number you wrote is true as to say between 1 and 10 it is valid 
+        }
+else
+{
+    Console.WriteLine("Invalid"); // If the number is not between 1 and 10 it is invalid
+}
